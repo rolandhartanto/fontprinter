334c790316ffd33cd0b0aea108663bd1d5e510df
dickynovanto1103 <dickynovanto1103@gmail.com>
2018-01-21 20:04:07 +0700

    cleaning code

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -1,14 +1,3 @@
-/*
-To test that the Linux framebuffer is set up correctly, and that the device permissions
-are correct, use the program below which opens the frame buffer and draws a gradient-
-filled red square:
-
-retrieved from:
-Testing the Linux Framebuffer for Qtopia Core (qt4-x11-4.2.2)
-
-http://cep.xor.aps.anl.gov/software/qt4-x11-4.2.2/qtopiacore-testingframebuffer.html
-*/
-
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -33,26 +22,22 @@ void clearScreen(int jumlahBaris, int jumlahKolom, int warna){
     int x,y;
     for (y = 0; y < jumlahBaris; y++){
         for (x = 0; x < jumlahKolom; x++) {
-            location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                           (y+vinfo.yoffset) * finfo.line_length;
+            location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (y+vinfo.yoffset) * finfo.line_length;
 
-                if (vinfo.bits_per_pixel == 32) {
-                    //printf("a[%d][%d]: %d\n",absis,ordinat,a[absis][ordinat]);
+            if (vinfo.bits_per_pixel == 32) {
+            
+                *(fbp + location) = warna;// blue
+                *(fbp + location + 1) = warna;// green
+                *(fbp + location + 2) = warna;// red
+                *(fbp + location + 3) = 0;// transparency    
                 
-                    *(fbp + location) = warna;        // Some blue
-                    *(fbp + location + 1) = warna;     // A little green
-                    *(fbp + location + 2) = warna;    // A lot of red
-                    *(fbp + location + 3) = 0;      // No transparency    
-                    
-                }
+            }
         }
     }
 }
     
 int main()
 {
-    
-
     // Open the file for reading and writing
     fbfd = open("/dev/fb0", O_RDWR);
     if (fbfd == -1) {
@@ -128,24 +113,22 @@ int main()
                            (y+vinfo.yoffset) * finfo.line_length;
 
                 if (vinfo.bits_per_pixel == 32) {
-                    //printf("a[%d][%d]: %d\n",absis,ordinat,a[absis][ordinat]);
                     if(a[absis][ordinat]=='0'){
-                        *(fbp + location) = putih;        // Some blue
-                        *(fbp + location + 1) = putih;     // A little green
-                        *(fbp + location + 2) = putih;    // A lot of red
-                        *(fbp + location + 3) = 0;      // No transparency    
+                        *(fbp + location) = putih;// blue
+                        *(fbp + location + 1) = putih;// green
+                        *(fbp + location + 2) = putih;// red
+                        *(fbp + location + 3) = 0;// transparency    
                     }else{
-                        *(fbp + location) = hitam;        // Some blue
-                        *(fbp + location + 1) = hitam;     // A little green
-                        *(fbp + location + 2) = hitam;    // A lot of red
-                        *(fbp + location + 3) = 0;      // No transparency    
+                        *(fbp + location) = hitam;// blue
+                        *(fbp + location + 1) = hitam;// green
+                        *(fbp + location + 2) = hitam;// red
+                        *(fbp + location + 3) = 0;// transparency    
                     }
                     ordinat++;
-            //location += 4;
-                } else  { //assume 16bpp
+                } else  { 
                     int b = 10;
-                    int g = (x-100)/6;     // A little green
-                    int r = 31-(y-100)/16;    // A lot of red
+                    int g = (x-100)/6;// green
+                    int r = 31-(y-100)/16;// red
                     unsigned short int t = r<<11 | g << 5 | b;
                     *((unsigned short int*)(fbp + location)) = t;
                 }
@@ -157,9 +140,6 @@ int main()
         idxKolom+=32;
         if(idxKolom>=1920){idxKolom = 0; idxBaris+=34;} //jika melebihi pixel column..lanjutkan di baris selanjutnya dan idxKolom direset jadi 0
     }
-    
-    
-
     munmap(fbp, screensize);
     close(fbfd);
     return 0;
